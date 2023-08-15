7ba4c3a2c789a18d0271d5765b4841dca8212bc9
Abhilasha Raghuwanshi <araghuwanshi511@gmail.com>
2023-08-14 23:46:07 -0400

    auth

---
+++ passportConfig.js
@@ -0,0 +1,52 @@
+const LocalStrategy = require("passport-local").Strategy;
+const { authenticate } = require("passport");
+const { pool } = require("./dbConfig");
+const argon2 = require('argon2');
+
+function initialize(passport) {
+    const authenticateUser = async (username, password, done) => {
+        try {
+            const results = await pool.query(
+                `SELECT * FROM users WHERE email = $1`, [username]
+            );
+
+            if (results.rows.length > 0) {
+                const user = results.rows[0];
+                const isMatch = await argon2.verify(user.password, password);
+
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: "Password is not correct" });
+                }
+            } else {
+                return done(null, false, { message: "Username is not registered" });
+            }
+        } catch (err) {
+            return done(err);
+        }
+    };
+
+
+    passport.use(new LocalStrategy(
+        {
+            usernameField: "username",
+            passwordField: "password"
+        },
+        authenticateUser
+        )
+    );
+
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const results = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+            return done(null, results.rows[0]);
+        } catch (err) {
+            return done(err);
+        }
+    });
+}
+
+module.exports = initialize;
---
+++ server.js
@@ -0,0 +1,135 @@
+const express = require('express'); // import express framework
+const app = express(); // create instance of express
+const { pool } = require('./dbConfig');
+const argon2 = require('argon2'); // import argon2 for password hashing
+const session = require('express-session');
+const flash = require('express-flash');
+const passport = require('passport');
+
+const initializePassport = require('./passportConfig');
+initializePassport(passport);
+
+const port = 3000;
+const hostname = "localhost";
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+
+// serve the login.html file at the root path
+app.get("/", (req, res) => {
+    res.render("index");
+
+});
+
+app.get("/login", checkAuthenticated, (req, res) => {
+    res.render("login");
+
+});
+
+app.get("/register", checkAuthenticated, (req, res) => {
+    res.render("register");
+
+});
+
+app.get("/dashboard", checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", {user: req.user.name});
+
+});
+
+app.get("/logout", (req, res) => {
+    req.logOut();
+    req.flash('success_message', 'You have successfully logged out');
+    res.redirect('/login');
+});
+
+app.post('/register', async (req, res) => {
+    let {username, password, password2} = req.body;
+    console.log({username, password, password2});
+
+    let errors = [];
+
+    if (!username || !password || !password2) {
+        errors.push({message: 'Please enter all fields'});
+    }
+
+    if (password.length < 6) {
+        errors.push({message: "Password should be atleast 6 characters long"});
+    }
+
+    if (password != password2) {
+        errors.push({message: "Passwords do not match"});
+    }
+
+    if(errors.length > 0) {
+        res.render('register', {errors});
+    } else {
+        try{ 
+            // validation passed
+            let hashpassword = await argon2.hash(password);
+            
+            const results = await pool.query(
+                `SELECT * FROM users WHERE name = $1`, [username]
+            );
+        
+            console.log("here");
+            console.log(results.rows);
+        
+            if (results.rows.length > 0) {
+                errors.push({ message: "Username already in use" });
+                res.render('register', { errors });
+            } else {
+                pool.query(`INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id, password`, [username, hashpassword], (err, results) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(results.rows);
+                    req.flash('success_message', 'Your account has been registered. Please log in');
+                    res.redirect('/login');
+                })
+            }
+        } catch (err) {
+            // Handle errors
+            throw err;
+        }
+    }
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failiureRedirect: '/login',
+    failiureFlash: true
+}));
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/dashboard');
+    }
+    next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } 
+    res.redirect('/login');
+}
+
+// start the http server
+app.listen(port, hostname, () => {
+    console.log(`Listening at: http://${hostname}:${port}`);
+});
