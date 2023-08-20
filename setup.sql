c512ce5b4c46163a45ef7e48bc7a4ae95b8f8aec
Abhilasha Raghuwanshi <araghuwanshi511@gmail.com>
2023-08-20 18:08:45 -0400

    add setup.sql

---
+++ setup.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE unologin;
+\c unologin
+CREATE TABLE users (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(200) NOT NULL,
+	password VARCHAR(200) NOT NULL,
+    UNIQUE (name)
+);
